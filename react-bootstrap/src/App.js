dfa6092321233d836e1be20662f21d0f211c1290
J.Grids <88521380+GridsETH2@users.noreply.github.com>
2022-02-28 22:55:22 +0700

    update

--- react-bootstrap/src/App.js
+++ react-bootstrap/src/App.js
@@ -1,97 +1,34 @@
-import './App.css';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ProTip from './ProTip';
+import Footer from './components/footer';
 
-import 'bootstrap/dist/css/bootstrap.css';
-
-import Nav from 'react-bootstrap/Nav';
-import NavBar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from "react-router-dom";
-
-import CreateUser from './components/create-user.component'
-import EditUser from './components/edit-user.component'
-import UserList from './components/user-list.component'
-
-function App() {
+function Copyright() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-
-          <NavBar bg="dark" variant="dark">
-
-            <Container>
-
-              <NavBar.Brand>
-
-                <Link to={"/create-user"} className="nav-link">
-
-                  React MERN Stack CRUD
-
-                </Link>
-
-              </NavBar.Brand>
-
-              <Nav className="justify-content-end">
-
-                <Nav>
-
-                  <Link to={"/create-user"} class="nav-link">
-
-                    Create USER. 
-
-                  </Link>
-
-                </Nav>
-
-                <Nav>
-
-                  <Link to={"/user-list"} class="nav-link">
-
-                    USER List.
-
-                  </Link>
-
-                </Nav>
-
-              </Nav>  
-
-            </Container>
-
-          </NavBar>
-          
-        </header>
-
-        <Container>
-
-          <Row>
-
-            <Col md="12">
-            
-              <div className="wrapper">
-
-                  <Route exact path="/" component={CreateUser} />
-                  <Route path="/create-user" component={CreateUser} />
-                  <Route path="/edit-use:id" component={EditUser} />
-                  <Route path="/user-list" component={UserList} />
-
-              </div>
-            
-            </Col>
-
-          </Row>
-
-        </Container>
-
-      </div>
-    </Router>
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App example with styled-components
+        </Typography>
+        <ProTip />
+        <Copyright />
+        <Footer />
+      </Box>
+    </Container>
+  );
+}
